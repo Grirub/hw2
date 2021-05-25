3ca7e2c30f21eb88441cd259c93bf0ab0172bbda
grisha <g.rubinchik@uni21.org>
2021-05-25 18:48:50 +0300

    hw2

---
+++ hw3/src/task1.java
@@ -0,0 +1,15 @@
+public class task1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int w = 7;
+        int r = 2;
+        int z = x/y;
+        int t = w%r;
+        int b = 9;
+        double m = Math.sqrt(b);
+        System.out.println(z);
+        System.out.println(t);
+        System.out.println(m);
+    }
+}
