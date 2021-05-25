f94201faac8581c64c0dcbcbbc2ace11c2d3e945
grisha <g.rubinchik@uni21.org>
2021-05-25 20:02:45 +0300

    hw3-2

--- hw3-1/src/task3.java
+++ hw3-1/src/task3.java
@@ -1,7 +1,9 @@
 public class task3 {
     public static void main(String[] args) {
-        float n = 10.9f;
+        float n = 10.7f;
         float y = Math.round (n);
-        System.out.println(y);
+        System.out.println(Math.round (n));
+        System.out.println(Math.floor  (n));
+        System.out.println(Math.ceil (n));
     }
 }
