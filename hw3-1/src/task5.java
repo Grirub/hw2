578da2fd9e5d89f8ecf792b8d335268f89c05001
grisha <g.rubinchik@uni21.org>
2021-05-25 20:18:06 +0300

    hw3-2

---
+++ hw3-1/src/task5.java
@@ -0,0 +1,8 @@
+public class task5 {
+    public static void main(String[] args) {
+        final int h = 5;
+        final int w = 3;
+        int s = (h+w)*2;
+        System.out.println(s);
+    }
+}
