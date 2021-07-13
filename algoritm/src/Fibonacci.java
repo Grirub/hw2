ef0dd78b2a6c94514ab81541141e257698df2de0
grisha <g.rubinchik@uni21.org>
2021-07-13 18:30:00 +0300

    y

--- algoritm/src/Fibonacci.java
+++ algoritm/src/Fibonacci.java
@@ -1,16 +1,16 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(findFibonaccirecursive(5));
+        System.out.println(findFibonacciRecursive(5));
         System.out.println(findFibonacci(10));
         System.out.println(findFibonacci(15));
         System.out.println(findFibonacci(25));
         System.out.println(findFibonacci(100));
     }
 
-    public static long findFibonaccirecursive(int n) {
+    public static long findFibonacciRecursive(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        return findFibonaccirecursive(n - 1) + findFibonaccirecursive(n - 2);
+        return findFibonacciRecursive(n - 1) + findFibonacciRecursive(n - 2);
     }
 
     public static long findFibonacci(int k) {
